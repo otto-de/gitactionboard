d7885bd5cbe9010423213811bb46852bf374c3cf
Suman Maity <sumanm@thoughtworks.com>
2021-12-04 10:40:01 +0530

    Upgrade talisman version

--- bin/install-talisman.sh
+++ bin/install-talisman.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TALISMAN_VERSION="1.11.0"
-TALISMAN_SHA256="8fbab0ab3dbbb1f64ba2b0040f2b4ef10e66eb0e2553c4fd80c3ba65063736ab"
+TALISMAN_VERSION="1.23.0"
+TALISMAN_SHA256="639535cb45c110bee36c4c16d59501d263a9c95a98294cf2e6f736dc63abbaaf"
 
 _main(){
  curl -fsSL -o ./talisman "https://github.com/thoughtworks/talisman/releases/download/v${TALISMAN_VERSION}/talisman_linux_amd64" &&
