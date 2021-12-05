9ae3fa4ab1d413c0806b8b9c6bb1f9aa992944c1
Suman Maity <sumanm@thoughtworks.com>
2021-12-05 18:59:38 +0530

    Introduce basic architecture fitness test

---
+++ backend/src/test/java/de/otto/platform/gitactionboard/architecture/ArchitectureFitnessTest.java
@@ -0,0 +1,31 @@
+package de.otto.platform.gitactionboard.architecture;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+
+@AnalyzeClasses(
+    packages = "de.otto.platform.gitactionboard",
+    importOptions = DoNotIncludeTests.class)
+class ArchitectureFitnessTest {
+
+  @ArchTest
+  static void domainDoesNotImportFromAdapterPackage(JavaClasses classes) {
+    noClasses()
+        .that()
+        .resideInAPackage("..domain..")
+        .should()
+        .dependOnClassesThat()
+        .resideInAPackage("..adapters..")
+        .check(classes);
+  }
+
+  @ArchTest
+  static void applicationDoesNotHaveCyclicDependencies(JavaClasses classes) {
+    slices().matching("..gitactionboard.(*)..").should().beFreeOfCycles().check(classes);
+  }
+}
