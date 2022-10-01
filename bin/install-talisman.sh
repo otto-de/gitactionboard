04e799db1e9c49824c13d5a102af902ebec84ea1
Suman Maity <sumanm@thoughtworks.com>
2022-10-01 12:31:41 +0530

    build: Update talisman checksum

--- bin/install-talisman.sh
+++ bin/install-talisman.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 TALISMAN_VERSION="1.29.1"
-TALISMAN_SHA256="f0beadec8603e668110739c19bc01a1877b0d3e73fb5171a83b0a4c08a091c6d"
+TALISMAN_SHA256="87b32e5fd48ab710b4d63fe0e5098612df54e9a8afdb9c57968ee63fb1a0449d"
 
 _main(){
  curl -fsSL -o ./talisman "https://github.com/thoughtworks/talisman/releases/download/v${TALISMAN_VERSION}/talisman_linux_amd64" &&
