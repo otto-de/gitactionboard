6ce2626c2eac2437cffd711239a94d65f98e8f34
Suman Maity <sumanm@thoughtworks.com>
2022-12-26 20:49:52 +0100

    build: Update hadolint version to v2.12.0

--- bin/install-hadolint.sh
+++ bin/install-hadolint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HADOLINT_VERSION="2.10.0"
+HADOLINT_VERSION="2.12.0"
 
 _main(){
  curl -fsSL -o ./hadolint "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64" &&
