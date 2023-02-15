686f0af8f1f935b1490cbb19a56b8bf09e95e7cc
Suman Maity <sumanm@thoughtworks.com>
2023-02-15 22:17:16 +0100

    build: Update talisman version

--- bin/install-talisman.sh
+++ bin/install-talisman.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TALISMAN_VERSION="1.29.4"
-TALISMAN_SHA256="93aa6f36505a890f97883908dd4078e69a5d74b1f7d00410b7e1ad682c9f2c96"
+TALISMAN_VERSION="1.30.0"
+TALISMAN_SHA256="4b416431264bfe079f12faf71426ae7fbdd36b5c499c9b80b274b61d75f82059"
 
 _main(){
  curl -fsSL -o ./talisman "https://github.com/thoughtworks/talisman/releases/download/v${TALISMAN_VERSION}/talisman_linux_amd64" &&
