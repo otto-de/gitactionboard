4e345cb540bf9f9ba2aa0b5ec63ec969c251cc6c
Suman Maity <sumanm@thoughtworks.com>
2023-10-25 21:23:52 +0200

    chore: Fix spotbugs warnings

--- backend/src/main/java/de/otto/platform/gitactionboard/domain/workflow/Status.java
+++ backend/src/main/java/de/otto/platform/gitactionboard/domain/workflow/Status.java
@@ -1,5 +1,8 @@
 package de.otto.platform.gitactionboard.domain.workflow;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings("PI_DO_NOT_REUSE_PUBLIC_IDENTIFIERS_CLASS_NAMES")
 public enum Status {
   EXCEPTION,
   SUCCESS,
